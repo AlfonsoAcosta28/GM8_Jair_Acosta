caf38c9d22aa1082c2c4c38af657d162f8fdc7d7
AlfonsoAcosta28 <109112791+AlfonsoAcosta28@users.noreply.github.com>
2023-10-17 20:47:00 -0600

    Home

--- GymManagerFrontend/src/app/app-routing.module.ts
+++ GymManagerFrontend/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { InitLayoutComponent } from './share/init-layout/init-layout.component';
 import { AdminLayoutComponent } from './share/admin-layout/admin-layout.component';
 import { HasSessionGuard } from './core/guards/has-session.guard';
+import { HomeComponent } from './pages/home/home.component';
+import { InitHomeComponent } from './share/init-home/init-home.component';
 
 const routes: Routes = [
-  {path: '', redirectTo:'/sign-in', pathMatch: 'full'},
+  {path: '', redirectTo:'/init-home', pathMatch: 'full'},
   {path: '', component: InitLayoutComponent, children:[
     { path: 'sign-in', loadChildren: () => import('./pages/sign-in/sign-in.module').then(m => m.SignInModule) },
     { path: 'sign-up', loadChildren: () => import('./pages/sign-up/sign-up.module').then(m => m.SignUpModule) },
@@ -16,6 +18,9 @@ const routes: Routes = [
     { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule), canActivate:[HasSessionGuard] },
     { path: 'users', loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule) ,canActivate:[HasSessionGuard] },
     { path: 'not-found', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) },
+  ]},
+  {path: '', component: InitHomeComponent, children:[
+    { path: 'init-home', loadChildren: () => import('./share/init-home/init-home.module').then(m => m.InitHomeModule) },
   ]},
   {path: '**', redirectTo:'/not-found', pathMatch: 'full'},
 ];
